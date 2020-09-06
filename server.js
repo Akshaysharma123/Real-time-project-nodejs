23f94943875562d30c4d66ec8262816794535d46
Akshaysharma123 <otayson3@gmail.com>
2020-09-06 16:39:56 +0530

    page complate

--- server.js
+++ server.js
@@ -5,15 +5,28 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
 
 
 // set Templates engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+})
 
 // assets 
 app.use(express.static('public'));
